3dd146529f038ab5e4271db495559b57729032f0
montionprojectr <montionprojectr@gmail.com>
2024-03-05 12:37:10 +0700

    up code

--- pages-adm-jurnal-dkelasb.php
+++ pages-adm-jurnal-dkelasb.php
@@ -25,7 +25,7 @@ $id_th_pelajaran = $_POST['id_th_pelajaran'];
 </thead>
 <tbody>
  <?php
-$query = mysqli_query($koneksi, "select id_jurnal, concat_ws(' - ', name_kelas, singkat_jurusan, rombel) as kelas , x.kode_mapel, name_mapel, nama_lengkap, hari_dantgl, concat_ws(' - ', time_mulai, time_akhir) as wkt_kehadiran, hadir, alpa, ijin, sakit, jumlah from tb_jurnal_kelas x inner join tb_user y on y.id_user = x.id_user inner join tb_mapel z on z.kode_mapel = x.kode_mapel inner join tb_kel_jur_rombel a on a.id_rombel = x.id_rombel inner join tb_kelas b on b.id_kelas = a.id_kelas inner join tb_jurusan c on c.id_jurusan = a.id_jurusan where x.id_rombel = '".$id_rombel."' and hari_dantgl = '".$date_day."' and id_th_pelajaran = '".$id_th_pelajaran."'");
+$query = mysqli_query($koneksi, "select id_jurnal, concat_ws(' - ', name_kelas, singkat_jurusan, rombel) as kelas , x.kode_mapel, name_mapel, nama_lengkap, hari_dantgl, concat_ws(' - ', time_mulai, time_akhir) as wkt_kehadiran, hadir, alpa, ijin, sakit, jumlah from tb_jurnal_kelas x inner join tb_user y on y.id_user = x.id_user inner join tb_mapel z on z.kode_mapel = x.kode_mapel inner join tb_kel_jur_rombel a on a.id_rombel = x.id_rombel inner join tb_kelas b on b.id_kelas = a.id_kelas inner join tb_jurusan c on c.id_jurusan = a.id_jurusan where x.id_rombel = '".$id_rombel."' and hari_dantgl = '".$date_day."' and id_th_pelajaran = '".$id_th_pelajaran."' group by id_jurnal asc");
 while ($data = mysqli_fetch_array($query)) { ?>
 	<tr>
 		<td><?= $data['id_jurnal']; ?></td>
